3e7d7c2ca2b56217c96e029c34a60fa0bbf9629f
shrirangop <shrirangelk221@gmail.com>
2023-12-18 16:23:57 +0530

    done with basic calculator

--- script.js
+++ script.js
@@ -4,25 +4,49 @@ let string = "";
 let numarr = [];
 let numstring = "";
 let ans = 0;
+count = 1;
 
 const calci = (event)=>{
     Enteredval = event.target.textContent;
     numstring+=Enteredval;
-    if(Enteredval==="+"){
-    numarr.push(parseFloat(numstring));
-    ans+=parseFloat(numstring);
-    numstring = "";
+
+    // if(Enteredval==="+"){
+    // console.log(parseFloat(numstring));
+    // numarr.push(parseFloat(numstring));
+    // ans+=parseFloat(numstring);
+    // numstring = "";
+
     
+    // }
+
+    // if(Enteredval==="-"){
+    //     numarr.push(parseFloat(numstring));
+    //     ans-=parseFloat(numstring);
+    //     numstring = "";
+    // }
+
+    // if(Enteredval==="*"){
+    //     numarr.push(parseFloat(numstring));
+    //     ans*=parseFloat(numstring);
+    //     numstring = "";
+    // }
+
+    // if(Enteredval==="/"){
+    //     numarr.push(parseFloat(numstring));
+    //     ans/=parseFloat(numstring);
+    //     numstring = "";
+    // }
+
+    if(Enteredval==="="){
+    document.getElementById("screen").innerText = eval(string);
+    console.log(ans);
     }
+
+    else{
     string+=Enteredval;
     document.getElementById("screen").innerText = string;
+    }
    
-    console.log(numarr);
-    console.log(ans);
-
-
-
-
 }
 
 document.addEventListener('click',calci);
